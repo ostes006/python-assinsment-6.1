495a60ad12077941bea7fdcfabe57442cc865238
ostes006 <124544629+ostes006@users.noreply.github.com>
2023-02-17 03:27:32 +0000

    changed kilometers to feet

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ def main():
 	# Get the number of miles.
 	steps_needed = float(input('Enter the number of steps: '))
 	# Convert the miles to kilometers.
-	steps_to_kilometers(miles_needed)
+	steps_to_feet (steps_needed)
 
 # The intro function displays an Introductory screen.
 def intro():
 	print('This program converts measurements')
-	print('in steps to kilometers. For your')
+	print('in steps to feet. For your')
 	print('references the formula is:')
-	print(' 1 miles = 1.609344 kilometers')
+	print(' 1 steps = 1.609344 feet')
 	print()
 
 # The miles_to_kilometers function accepts a number of
 # miles and displays the equivalent number of kilometers.
-def steps_to_kilometers(miles):
-	kilometers = miles * 1.609344
-	print('That converts to', kilometers, 'kilometers.')
+def steps_to_feet(steps):
+	feet = steps * 1.609344
+	print('That converts to', feet, 'feet.')
 
 # Call the main function
 main()
